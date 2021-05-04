f36a71a79a134cd33f21caa5686aa8d9a7f921d6
Himeth Dahanayake <himethdaha@gmail.com>
2021-05-03 23:56:08 -0400

    Create Page

---
+++ Pages/JournalList/Create.cshtml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPage.Models;
+
+namespace RazorPage.Pages.JournalList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Journal journals { get; set; }
+        public void OnGet()
+        {
+        }
+    }
+}
